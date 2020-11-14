628a879ba0ae9c5d60a54dba09d8a76b15338b43
AliyanRazzaq <aliyanrazzaq18@gmail.com>
2020-11-14 05:55:31 +0500

    made signup screen few tweaks left

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+//import './index.css';
+import './signup.css';
 import Signin from './signin';
 import reportWebVitals from './reportWebVitals';
 
-
 ReactDOM.render(
   <React.StrictMode>
-    <Signin />
+    <Signin />  
   </React.StrictMode>,
   document.getElementById('root')
 );
